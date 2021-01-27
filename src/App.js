d5be4f6c1bbc6d4364fd2d8e8f1999157347e39e
Kevin Nguyen <ngynkvn@gmail.com>
2021-01-27 14:34:43 -0500

    Roomkey

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import randomWords from "random-words";
+
+var server = { urls: "stun:stun.stunprotocol.org:3478" };
+
+function generateCode() {
+  return randomWords(3).join('-');
+}
 
 function App() {
+  const [status, updateStatus] = useState("Loaded");
+  const [code, updateCode] = useState(generateCode());
+  const [inputCode, _updateInputCode] = useState(generateCode());
+
+  const updateInputCode = (e) => {
+    _updateInputCode(e.target.value);
+  }
+
+  const userSubmitCode = (e) => {
+    e.preventDefault();
+    console.log("Submitted: ", inputCode)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        {status}
+      </p>
+
+      <p>
+        Your code is: {code}
+      </p>
+      <div>
+        <form onSubmit={userSubmitCode}>
+          <input onChange={updateInputCode}></input>
+        </form>
+      </div>
     </div>
   );
 }
