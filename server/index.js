898033ad8d0efe0239dc66af65265416cc6a7581
rieckn <35986321+rieckn@users.noreply.github.com>
2021-01-27 23:15:43 -0500

    trying to connect database

--- server/index.js
+++ server/index.js
@@ -4,6 +4,24 @@ const path = require('path');
 const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
 
+const mysql = require("mysql");
+const fs = require('fs');
+const knex = require('knex')({
+    client: 'mysql',
+    version: '5.7',
+    connection: {
+      host : '35.196.27.6',
+      ssl: {
+        ca: fs.readFileSync('server-ca.pem'),
+        key: fs.readFileSync('client-key.pem'),
+        cert: fs.readFileSync('client-cert.pem'),
+      },
+      user : 'smoll-talk',
+      password : 'Pbeq0hcDejpO12cN',
+      database : 'smol_talk',
+    },
+});
+
 const app = express();
 
 // Priority serve any static files.
@@ -18,6 +36,8 @@ app.get('/api', function (req, res) {
 // All remaining requests return the React app, so it can handle routing.
 app.get('*', function(request, response) {
     response.sendFile(path.resolve(__dirname, '../react/build', 'index.html'));
+    knex('users')
+    .insert({name: 'john', groupID: '1'})
 });
 
 app.listen(PORT, function () {
