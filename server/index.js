7672bacfae11dd36d06470bf2bad7b96e51ab2e9
rieckn <35986321+rieckn@users.noreply.github.com>
2021-01-28 19:25:29 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(express.static(path.resolve(__dirname, '../react/build')));
 // All remaining requests return the React app, so it can handle routing.
 app.get('*', function(request, response) {
     response.sendFile(path.resolve(__dirname, '../react/build', 'index.html'));
-    connection.query('INSERT INTO users (name, groupID) VALUES (John, test)', function (error, results, fields) {
+    connection.query("INSERT INTO users (name, groupID) VALUES ('John', 'test')", function (error, results, fields) {
         if (error) throw error;
         console.log("Added to database");
     });
