6c40a226e1eaf12c2c8c6a44e4cdaf1bdf5c8a28
Carl Lin <lincarl@ufl.edu>
2021-02-02 11:59:18 -0500

    Acknowledged Shareable Link Display

--- src/Views/LandingPage.js
+++ src/Views/LandingPage.js
@@ -1,11 +1,13 @@
 import React, { useContext, useState } from "react";
 import { AppContext } from "../AppContext";
+import axios from "axios";
 
 import Generate from "../components/Generate";
 
 function Landing(props) {
     const [showLink, updateShowLink] = useState(false);
     const [name, updateName] = useState("");
+    // const [shareableLink, updateShareableLink] = useState("http://localhost:3000/room/");
 
     /* 
     Function That Executes When Generate Room Button Is Clicked
@@ -16,6 +18,10 @@ function Landing(props) {
     function handleGenerateClick(e) {
         appContext.dispatch({ type: "update-name", displayName: name });
         updateShowLink(true);
+        // axios.get(`http://localhost:3000/request-room`).then(res => {
+        //     console.log(res);
+        //     updateShareableLink(shareableLink + res);
+        // });
     }
 
     return (
@@ -24,7 +30,7 @@ function Landing(props) {
                 <input placeholder="Enter Display Name" onChange={(e) => updateName(e.target.value)} /> <br />
                 <button onClick={handleGenerateClick}>Generate Room</button>
             </div>
-            {showLink ? <Generate /> : null}
+            {showLink ? <Generate name={name} /> : null}
         </div>
     );
 }
