d31b77bd9aa4432ad081bf27051a4100ab0b1bc8
rieckn <35986321+rieckn@users.noreply.github.com>
2021-02-14 16:42:38 -0500

    Fixed Formatting

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import randomWords from "random-words";
+import ReactDOM from "react-dom";
+
+/* Views */
+import Join from "./components/Join";
+import Posts from "./components/Post";
+import PostContent from "./components/PostContent"
+import Landing from "./Views/LandingPage";
+import Room from "./Views/Room";
+
+/* Styling */
+import "./Styling/App.css";
+import { AppContext, ContextProvider } from "./AppContext";
+
+var server = { urls: "stun:stun.stunprotocol.org:3478" };
+
+function generateCode() {
+  return randomWords(3).join('-');
+}
+
+function App() {
+  return (
+    <ContextProvider>
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path={`/`}><Landing /></Route>
+            <Route exact path={`/room/:roomID/`} component={Room}></Route>
+          </Switch>
+        </BrowserRouter>
+        {/* <Posts />
+      <PostContent /> */}
+      </div>
+    </ContextProvider>
+  );
+}
+
+export default App;
---
+++ src/AppContext.js
@@ -0,0 +1,37 @@
+
+import React, { useReducer } from "react";
+export const AppContext = React.createContext()
+
+const initialState = {
+    userId: null,
+    displayName: null,
+    roomKey: null
+};
+const reducer = (state, action) => {
+    console.log(action);
+    switch (action.type) {
+        case 'join-room':
+            return {
+                ...state,
+                roomKey: action.roomKey,
+            };
+        case 'update-name':
+            return {
+                ...state,
+                displayName: action.displayName,
+            }
+        case 'update-user-id':
+            return {
+                ...state,
+                userId: action.userId,
+            }
+    }
+}
+export function ContextProvider({ children }) {
+    const [state, dispatch] = React.useReducer(reducer, initialState)
+    return (
+        <AppContext.Provider value={{ state, dispatch }}>
+            {children}
+        </AppContext.Provider>
+    )
+}
